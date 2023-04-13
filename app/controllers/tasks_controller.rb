5786a50bfb0fe9f2f76960a9e0563904edb1d0d5
Latoya Alford <lat.jamison@gmail.com>
2023-04-12 21:11:45 -0400

    implement edit route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,16 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+    @tasks.update(task_params)
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
